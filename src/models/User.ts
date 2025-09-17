02346c6736652d7f661cde3699e239b0e636cbbb
MOUHIB Otman <mouhib.otm@gmail.com>
2025-09-17 04:54:16 +0100

    Fix user role enum and other updates

--- src/models/User.ts
+++ src/models/User.ts
@@ -24,7 +24,7 @@ const UserSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['user', 'admin'],
+    enum: ['user', 'admin', 'visitor'],
     default: 'user',
   },
   createdAt: {
